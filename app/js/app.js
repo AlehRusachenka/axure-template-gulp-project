261b96511ef7b3ce7632d3a68a646d8a179d0a39
Aleh.R <aleh.rusachenka@gmail.com>
2022-06-22 12:09:31 +0300

    modal window modified tab section modified

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,26 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
+import Splide from "@splidejs/splide";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Splide(".review-slider", {
+    classes: {
+      arrow: "splide__arrow review-slider__arrow",
+    },
+    pagination: false,
+    type: "loop",
+    perPage: 1,
+    breakpoints: {
+      560: {
+        arrows: false,
+        pagination: true,
+        classes: {
+          pagination: "splide__pagination review-slider__pagination",
+        },
+      },
+    },
+  }).mount();
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   // Fixed Header
@@ -35,46 +56,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   themeToggle.addEventListener("change", switchTheme, false);
-
-  // Tabs switcher
-
-  // var tab;
-  // var tabContent;
-
-  // window.onload = function () {
-  //   tabContent = document.getElementsByClassName("tabContent");
-  //   tab = document.getElementsByClassName("tab");
-  //   hideTabsContent(1);
-  // };
-
-  // document.getElementById("tabs").onclick = function (event) {
-  //   var target = event.target;
-  //   if (target.className == "tab") {
-  //     for (var i = 0; i < tab.length; i++) {
-  //       if (target == tab[i]) {
-  //         showTabsContent(i);
-  //         break;
-  //       }
-  //     }
-  //   }
-  // };
-
-  // function hideTabsContent(a) {
-  //   for (var i = a; i < tabContent.length; i++) {
-  //     tabContent[i].classList.remove("show");
-  //     tabContent[i].classList.add("hide");
-  //     tab[i].classList.remove("whiteborder");
-  //   }
-  // }
-
-  // function showTabsContent(b) {
-  //   if (tabContent[b].classList.contains("hide")) {
-  //     hideTabsContent(0);
-  //     tab[b].classList.add("whiteborder");
-  //     tabContent[b].classList.remove("hide");
-  //     tabContent[b].classList.add("show");
-  //   }
-  // }
 
   // Scroll to Anchor
 
